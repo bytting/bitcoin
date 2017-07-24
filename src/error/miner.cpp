0caca559e5867b50cc0168ce7aa655335444dd2e
Dag Robole <dag.robole@gmail.com>
2017-07-24 20:21:50 +0200

    Added copyright notice and inclusion guards

--- src/error/miner.cpp
+++ src/error/miner.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2017 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "miner.h"
 #include <string>
--- src/error/validation.h
+++ src/error/validation.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2017 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_ERROR_VALIDATION_H
+#define BITCOIN_ERROR_VALIDATION_H
 
 #include <system_error>
 
@@ -13,3 +19,5 @@ template<>
 }
 
 std::error_code make_error_code(ErrorValidation);
+
+#endif
